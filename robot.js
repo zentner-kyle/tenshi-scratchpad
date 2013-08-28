11921484bd942eb3befa15e84bb955af3d119310
Kyle Zentner <zentner.kyle@gmail.com>
2013-08-28 13:25:40 -0700

    Added collision detection.

--- robot.js
+++ robot.js
@@ -1,10 +1,15 @@
 
 var robot = {}
-robot.x = 400.0
-robot.y = 200.0
+robot.x = 50.0
+robot.y = 475.0
+
+// Midway through the field
+//robot.x = 400.0
+//robot.y = 275.0
+
 robot.theta = 0.0
 robot.width = 10.0
-robot.lspeed = .09
+robot.lspeed = .1
 robot.rspeed = .1
 
 function drive(robot, delta_t) {
@@ -36,8 +41,54 @@ for (var i = 0; i <= maxAngle; i++) {
   robotImages[i].id = 'robot-image'
   }
 
+var robotState = {
+  RUNNING: 0,
+  CRASHED: 1,
+  FINISHED: 2
+  }
+
+function CollisionChecker(imageName) {
+  var canvas = document.createElement('canvas')
+  var ctx = canvas.getContext('2d')
+  var image = new Image ( )
+  image.src= imageName
+
+  image.onload = function () {
+    canvas.width = image.width
+    canvas.height = image.height
+    ctx.drawImage(image, 0, 0)
+    }
+
+  this.getStatus = function(x, y) {
+    var pixelData = ctx.getImageData(x, y, 1, 1).data
+    if (pixelData[3] == 0) {
+      return robotState.RUNNING
+      }
+    else if (pixelData[1] == 0xff &&
+             pixelData[0] == 0    &&
+             pixelData[2] == 0) {
+      return robotState.FINISHED
+      }
+    else {
+      return robotState.CRASHED
+      }
+    }
+  }
+
+var mazeName = 'mazes/S.png'
+var maze = document.getElementById('maze')
+if (maze != null) {
+  maze.src = mazeName
+  }
+
+var collisionCheck = new CollisionChecker(mazeName)
+var done = false
+
 var waitTime = 15
 function updateRobotPosition (deltaT) {
+  if (done) {
+    return
+    }
   var robot_div = document.getElementById('robot')
   robot_div.style.left = robot.x + 'px'
   robot_div.style.top  = robot.y + 'px'
@@ -45,5 +96,14 @@ function updateRobotPosition (deltaT) {
   var robot_image = document.getElementById('robot-image')
   robot_div.replaceChild(robotImages[degrees], robot_image)
   drive(robot, deltaT)
+  var state = collisionCheck.getStatus(robot.x, robot.y)
+  if (state == robotState.CRASHED) {
+    alert('The robot has crashed.')
+    done = true
+    }
+  else if (state == robotState.FINISHED) {
+    alert('Congratulations! You got to the end!')
+    done = true
+    }
   }
 window.setInterval(updateRobotPosition, waitTime, waitTime)
