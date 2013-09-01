09be4b68cf4d168e95ac5cf84372c5f01e934208
Kyle Zentner <zentner.kyle@gmail.com>
2013-08-31 23:34:08 -0700

    Parser now sets up pattern tables.

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-var requirejs = require('./r.js/r.js');
+var requirejs = require ('./r.js/r.js');
 
-requirejs.config({
+requirejs.config ({
   nodeRequire: require
   });
 
-requirejs(['./xregexp/xregexp-all.js'],
+requirejs (['./xregexp/xregexp-all.js'],
 function (xregexp) {
 
   var lex = function () {
-    var token_reg = xregexp( 
+    var token_reg = xregexp ( 
         ' (?<space>       \\p{Whitespace}+)                   |' +
         ' (?<number>      [0-9]+)                             |' +
         ' (?<identifier>  [\\p{Letter}_] [\\p{Letter}_0-9]*)  |' +
         ' (?<operator>    [^\\p{Letter}_0-9\\p{Whitespace}]+)  '
         , 'x' );
     return function (text) {
-      return xregexp.exec(text, token_reg);
+      return xregexp.exec (text, token_reg);
       }
-    } ()
+    } ();
 
   function getType (match) {
     var types = ['space', 'number', 'identifier', 'operator'];
@@ -26,8 +26,15 @@ function (xregexp) {
         return types[t];
         }
       }
-    throw new Error('Could not determine type of match "' + match[0] + '"');
-    }
+    throw new Error ('Could not determine type of match "' + match[0] + '"');
+    };
+
+  function make_special (name) {
+    return {
+      type: 'special',
+      text: name,
+      };
+    };
 
   function make_parser (states, startState) {
     var text = "";
@@ -35,6 +42,7 @@ function (xregexp) {
     var state = startState || "start";
     var tokenTable = {};
     var theStates = {};
+    var accum = make_ast_node ('(root)');
     for (var i in states) {
       theStates[states[i].name] = states[i];
       }
@@ -46,16 +54,24 @@ function (xregexp) {
         state = theStates[stateName];
         },
       lexOnce: function () {
-        var match = self.getState().lex (text.substr(idx));
-        var type = getType(match);
+        var match = self.getState ().lex (text.substr (idx));
+        if (match === null) {
+          return make_special ('(end)');
+          }
+        var type = getType (match);
         var tokenText = match[0];
-        idx += tokenText.length;
         match = self.getState ().getToken (type, tokenText);
+        if (match) {
+          idx += match.text.length;
+          }
+        else {
+          idx += tokenText.length;
+          }
         if (match === undefined) {
           match = tokenTable[match];
           }
         if (match === undefined) {
-          match = make_atom(tokenText, type);
+          match = make_atom (tokenText, type);
           }
         return match;
         },
@@ -63,41 +79,59 @@ function (xregexp) {
         text = some_text;
         idx = 0;
         },
+      parse: function () {
+        var token;
+        accum.push (make_special ('(start)'));
+        while (true) {
+          token = self.lexOnce ();
+          if (token !== null) {
+            accum.push (token);
+            if (token.text === '(end)') {
+              return accum;
+              }
+            }
+          }
+        },
       };
     return self;
-    }
+    };
 
   function make_state (name) {
     var self = {
       table: {},
       name: name || "anonymous state",
-      onMissing: function (toFind) {throw new Error("Could not find " + toFind);},
       lex: lex,
       getToken: function (type, text) { 
         if (type === 'space') {
           return null;
           }
+        if (type === 'operator') {
+            return self.table[longest_prefix(text, self.table)];
+          }
         return self.table[text];
         }
       };
     return self;
-    }
+    };
 
-  function make_pattern (matchers) {
+  function make_pattern (text, matchers) {
     return {
+      type: 'pattern',
+      text: text,
+      idx: 0,
       matchers: matchers || [],
       post: function (parser) {}
       };
-    }
+    };
 
-  function make_unary_match (token) {
+  function make_literal (token) {
     return {
       type: 'literal',
       arity: 'single',
       post: function (parser) {},
       token: token,
       };
-    }
+    };
 
   function make_left (lbp) {
     return {
@@ -106,27 +140,84 @@ function (xregexp) {
       post: function (parser) {},
       power: lbp,
       };
-    }
+    };
 
-  function make_right (rbp, arity) {
+  function make_right (rbp, arity, next_state) {
     return {
       type: 'right',
       arity: arity | 'single',
       post: function (parser) {},
       power: rbp,
       };
-    }
+    };
 
   function make_atom (text, type) {
     return {
       type: 'atom',
       subtype: type,
       text: text,
+      };
+    };
+
+  function make_ast_node (name, children) {
+    var self = {
+      name: name || "(ast)",
+      children: children || [],
+      push: function (token) {
+        self.children.push(token);
+        },
+      };
+    return self;
+    };
+
+  function longest_prefix (text, table) {
+    var i = text.length;
+    while (!(text.substr (0, i) in table) && i > 0) {
+      i -= 1;
+      }
+    if (i === 0) {
+      return undefined;
+      }
+    else {
+      return text.substr (0, i);
       }
     }
 
-  function fill_operator_table (table) {
-    // The following was basically copied from Wick
+  function fill_prefix_table (table) {
+    // The following was copied from Wick
+    var prefix_table = [
+        [ "+"       , 60 ] , 
+        [ "-"       , 60 ] , 
+        [ "*"       , 70 ] , 
+        [ "!"       , 40 ] , 
+        [ "!!"      , 40 ] , 
+        [ "\\"      , 10 ] , 
+        [ "\\\\"    , 10 ] , 
+        [ "@"       , 20 ] , 
+        [ "^"       , 40 ] , 
+        [ "&"       , 40 ] , 
+        [ "|"       , 40 ] , 
+        [ "~"       , 40 ] , 
+        [ ".."      , 60 ] , 
+        [ "$"       , 20 ] , 
+        [ "if"      , 13 ] ,
+        [ "for"     , 13 ] ,
+        [ "import"  , 13 ] ,
+        [ "while"   , 13 ] ,
+        [ "var"     , 13 ] ,
+        [ "assert"  , 13 ] ,
+      ];
+
+    for (var i in prefix_table) {
+      table[prefix_table[i][0]] = make_pattern(prefix_table[i][0], [
+          make_literal (prefix_table[i][0]),
+          make_right (prefix_table[i][2], 'single', 'prefix'),
+          ]);
+      }
+    };
+
+  function fill_infix_table (table) {
+    // The following was copied from Wick
     var infix_table = [
         [ "="    ,  5 ,  4 ,  true ] , 
         [ "<-"   , 10 ,  9 ,  true ] , 
@@ -175,93 +266,24 @@ function (xregexp) {
         [ "$"    , 20 , 20 , false ] , 
       ];
 
-    var prefix_table = [
-        [ "+"       , 60 ] , 
-        [ "-"       , 60 ] , 
-        [ "*"       , 70 ] , 
-        [ "!"       , 40 ] , 
-        [ "!!"      , 40 ] , 
-        [ "\\"      , 10 ] , 
-        [ "\\\\"    , 10 ] , 
-        [ "@"       , 20 ] , 
-        [ "^"       , 40 ] , 
-        [ "&"       , 40 ] , 
-        [ "|"       , 40 ] , 
-        [ "~"       , 40 ] , 
-        [ ".."      , 60 ] , 
-        [ "$"       , 20 ] , 
-        [ "if"      , 13 ] ,
-        [ "for"     , 13 ] ,
-        [ "import"  , 13 ] ,
-        [ "while"   , 13 ] ,
-        [ "var"     , 13 ] ,
-        [ "assert"  , 13 ] ,
-      ];
-
     for (var i in infix_table) {
-      var inf = infix_table[i];
-      table[inf[0]] = [operator('infix', inf[0], inf[1], inf[2], inf[3])];
+      table[infix_table[i][0]] = make_pattern(infix_table[i][0], [
+          make_left (infix_table[i][1]),
+          make_literal (infix_table[i][0]),
+          make_right (infix_table[i][2], 'single',
+                      infix_table[i][3] ? 'indent' : 'prefix'),
+          ]);
       }
-
-    for (var i in prefix_table) {
-      var pre = prefix_table[i];
-      var op = operator('prefix', pre[0], 0, pre[1], false);
-      if (table[pre[0]]) {
-        table[pre[0]].push(op);
-        }
-      else {
-        table[pre[0]] = [op];
-        }
-      }
-    }
-
-  function process_match (match, parser, callback) {
-    if (match.space) {
-      callback(token('space', match.space));
-      }
-    else if (match.number) {
-      callback(token('number', match.number));
-      }
-    else if (match.identifier) {
-      if (!(match.identifier in parser.table)) {
-        parser.table[match.identifier] = token('identifier', match.identifier);
-        }
-      return parser.table[match.identifier];
-      }
-    else if (match.operator) {
-      var ops = split_op(match.operator, parser.table);
-      for (var i in ops) {
-        callback(select_op(parser, ops[i]));
-        }
-      }
-    }
-
-  function split_op(text, table) {
-    var i = text.length;
-    while (!(text.substr(0, i) in table)) {
-      i -= 1;
-      }
-    if (i === 0) {
-      throw 'Could not identify operator ' + text;
-      }
-    if (i === text.length) {
-      return [text];
-      }
-    else {
-      return [text.substr(0, i)].concat(split_op(text.substr(i), table));
-      }
-    }
-
+    };
 
   function main () {
-    var infix_state = make_state('infix');
-    var parser = make_parser ([infix_state], 'infix');
-    parser.start("a + 1");
-    console.log(parser.lexOnce());
-    console.log(parser.lexOnce());
-    console.log(parser.lexOnce());
-    console.log(parser.lexOnce());
-    console.log(parser.lexOnce());
+    var infix_state = make_state ('infix');
+    var prefix_state = make_state ('prefix');
+    var parser = make_parser ([infix_state, prefix_state], 'infix');
+    fill_infix_table (infix_state.table);
+    fill_prefix_table (prefix_state.table);
+    parser.start ("a + 1");
+    console.log(parser.parse ());
     };
   main ();
   });
