7fc6a0032493b73a8c11b4f6747a24558fad785d
Kyle Zentner <zentner.kyle@gmail.com>
2013-09-04 15:00:09 -0700

    Made type and text tables into stacks.

--- pratt.js
+++ pratt.js
@@ -1,4 +1,5 @@
 var requirejs = require ('./r.js/r.js');
+var util = require('util');
 
 requirejs.config ({
   nodeRequire: require
@@ -17,6 +18,9 @@ function (xregexp) {
     var types = ['space', 'number', 'identifier', 'operator'];
     return function (text) {
       var match = xregexp.exec (text, token_reg);
+      if (!match) {
+        return null;
+        }
       for (var t in types) {
         if (match[types[t]] !== undefined) {
           return {type: types[t], text: match[types[t]]};
@@ -45,15 +49,47 @@ function (xregexp) {
     return obja;
     }
 
+  function tableStackGet (stack, key) {
+    var i = stack.length - 1;
+    var res;
+    while (i >= 0) {
+      res = stack[i][key];
+      if (res !== undefined) {
+        return res;
+        }
+      else {
+        i -= 1;
+        }
+      }
+    return undefined;
+    };
+
+  function tableStackSetNew (stack, key, val) {
+    stack[stack.length - 1][key] = val;
+    };
+
+  function tableStackChange (stack, key, val) {
+    var i = stack.length - 1;
+    var res;
+    while (i >= 0) {
+      if (key in stack[i]) {
+        stack[i][key] = val;
+        return;
+        }
+      }
+    throw new Error('Could not change key ' + key + ' to ' + val + ' in ' + stack + '.');
+    };
+
   var atom = {
     lbp: 0,
-    nud: function () {
+    nud: function (parser) {
       return this;
       },
     };
 
   function infix (lbp, rbp) {
-    rbp = rbp === undefined ? rbp : lbp;
+    rbp = rbp !== undefined ? rbp : lbp;
+    console.log(rbp)
     return {
       led: function (parser, left) {
         this.left = left;
@@ -64,157 +100,161 @@ function (xregexp) {
       };
     };
 
+  function prefix (rbp) {
+    return {
+      nud: function (parser) {
+        this.right = parser.expression (rbp);
+        return this;
+        },
+      };
+    };
+  var keyword = { type: 'keyword' };
+
   var parser = {
-  current: null,
-  idx: 0,
-  textTable: {
-    '+': infix(1),
-    },
-  typeTable: {
-    'number' : atom,
-    'identifier': atom,
-    },
-  advance: function () {
-    this.current = this.tokens[this.idx];
-    this.idx += 1;
-    if (this.current === undefined) {
-      this.current = {text: '(end)', lbp: 0};
-      }
-    if (this.current.text in this.textTable) {
-      this.current = obj_or(Object.create(this.textTable[this.current.text]), this.current);
-      }
-    else if (this.current.type in this.typeTable) {
-      this.current = obj_or(Object.create(this.typeTable[this.current.type]), this.current);
-      }
-    else if (this.current.type === 'space') {
-      this.advance ();
-      }
-    },
-  expression: function (rbp) {
-    var left;
-    var t = this.current;
-    this.advance();
-    left = t.nud();
-    while (rbp < this.current.lbp) {
-      t = this.current;
+    textTables: [
+      ],
+    exprTextTable: {
+      '+': infix(60),
+      '-': obj_or(infix(60), prefix(80)),
+      '++': prefix(90),
+      'if': keyword,
+      'while': keyword,
+      },
+    typeTables: [
+      ],
+    exprTypeTable: {
+      'number' : atom,
+      'identifier': atom,
+      'space': null,
+      },
+    textTablePush: function (table) {
+      this.textTables.push (table);
+      },
+    typeTablePush: function (table) {
+      this.typeTables.push (table);
+      },
+    textTableGet: function (key) {
+      return tableStackGet(this.textTables, key);
+      },
+    typeTableGet: function (key) {
+      return tableStackGet(this.typeTables, key);
+      },
+    textTableSetNew: function (key, val) {
+      tableStackSetNew (this.textTables, key, val);
+      },
+    typeTableSetNew: function (key, val) {
+      tableStackSetNew (this.typeTables, key, val);
+      },
+    splitOp: function (token) {
+      var i = token.text.length;
+      while (this.textTableGet(token.text.substr(0, i)) === undefined) {
+          i -= 1;
+          }
+      if (i === 0) {
+          throw 'Could not identify operator ' + token.text;
+          }
+      if (i === token.text.length) {
+          return [token];
+          }
+      else {
+          return [obj_or(token, {text: token.text.substr(0, i)})].concat(
+                  this.splitOp(obj_or(token, {text: token.text.substr(i)})));
+          }
+      },
+    moreTokens: function (count) {
+      while (count > 0) {
+        var token = lex(this.text.substr(this.text_idx));
+        if (token) {
+          this.text_idx += token.text.length;
+          if (token.type === 'operator') {
+            this.tokens.concat (this.splitOp(token));
+            }
+          else {
+            this.tokens.push (token);
+            }
+          }
+        count -= 1;
+        }
+      },
+    nextToken: function () {
+      if (this.tokens.length === this.idx) {
+        this.moreTokens(1)
+        }
+      this.current = this.tokens[this.idx];
+      this.idx += 1;
+      },
+    peekToken: function (idx) {
+      var count;
+      idx = idx || 0;
+      count = 1 + idx + this.idx - this.tokens.length;
+      if (count > 0) {
+        this.moreTokens(count);
+        }
+      return this.tokens[this.idx + idx];
+      },
+    loadCurrentFromTable: function () {
+      var toClone;
+      if (!this.current) {
+        this.current = {text: '(end)', type: '(special)', lbp: 0};
+        return;
+        }
+      toClone = this.textTableGet(this.current.text);
+      if (toClone !== undefined) {
+        this.current = obj_or(Object.create(toClone), this.current);
+        }
+      else {
+        toClone = this.typeTableGet(this.current.type);
+        if (toClone === null) {
+          this.advance ();
+          }
+        else if (toClone === undefined){
+          throw new Error('Unknown token!');
+          }
+        else {
+          this.current = obj_or(Object.create(toClone), this.current);
+          }
+        }
+      },
+    advance: function () {
+      this.nextToken ();
+      this.loadCurrentFromTable ();
+      },
+    expression: function (rbp) {
+      var left;
+      var t = this.current;
       this.advance();
-      left = t.led(this, left);
-      }
-    return left;
-    },
-    parse: function (text) {
+      left = t.nud(this);
+      while (rbp < this.current.lbp) {
+        t = this.current;
+        this.advance();
+        left = t.led(this, left);
+        }
+      return left;
+      },
+    parse_expr: function (text) {
+      this.typeTablePush(this.exprTypeTable);
+      this.textTablePush(this.exprTextTable);
       this.idx = 0;
       this.tokens = lexAll(text);
       this.advance ()
       return this.expression (0)
       }
+  };
+
+  function make_parser (text) {
+    return obj_or (Object.create (parser), {
+      idx: 0,
+      tokens: [],
+      current: null,
+      text_idx: 0,
+      text: text || "",
+      text_exhausted: true,
+      });
     };
 
-  console.log (parser.parse ('a + 1'));
-  //var token;
-  //var idx = 0;
+  function parse_expr (text) {
+    return make_parser ().parse_expr (text)
+    }
 
-  //function advance () {
-    //token = tokens[idx];
-    //idx += 1;
-    //if (token === undefined) {
-      //token = {text: '(end)', lbp: 0};
-      //}
-    //if (token.text in symbol_table) {
-      //obj_or(token, symbol_table[token.text]);
-      //}
-    //if (token.type === 'number') {
-      //token.nud = function () {
-        //return this;
-        //}
-      //token.lbp = 0;
-      //}
-    //else if (token.type === 'space') {
-      //advance ();
-      //}
-    //};
-
-  //var expression = function (rbp) {
-    //var left;
-    //var t = token;
-    //advance();
-    //left = t.nud();
-    //while (rbp < token.lbp) {
-      //t = token;
-      //advance();
-      //left = t.led(left);
-      //}
-    //return left;
-    //}
-
-  //var symbol_table = {
-    //'+': {
-      //led: function (left) {
-        ////console.log('HERE')
-        //this.left = left;
-        //this.right = expression (1);
-        //return this;
-        //},
-      //lbp: 1,
-      //}
-    //};
-
-  //var tokens = lexAll ('1 + 1');
-  //advance ();
-
-  //var original_symbol = {
-    //nud: function () {
-      //this.error ("Undefined.");
-      //},
-    //led: function (left) {
-      //this.error ("Missing operator.");
-      //}
-    //};
-
-  //function obj_or(obja, objb) {
-    //var k;
-    //for (k in objb) {
-      //obja[k] = objb[k];
-      //}
-    //return obja;
-    //}
-
-  //var parser_proto = {
-    //get: function (text) {
-      //return this.table[text]
-      //},
-    //parse: function (text) {
-      //this.text = text;
-      //this.idx = 0;
-      //},
-    //};
-
-  //function atom (parser, token) {
-    //return token;
-    //}
-
-  //function make_parser () {
-    //return obj_or (Object.create (parser_proto), {
-      //typeTable: {
-        //space: null,
-        //number: atom,
-        //identifier: atom,
-        //},
-      //text: "",
-      //idx: 0,
-      //rbp: 0,
-      //});
-    //};
-
-
-  //function parse (text) {
-    //return make_parser ().parse (text);
-    //}
-
-  //var parser 
-  //var expression = function (rbp) {
-    //var left;
-    //};
-
-
+  console.log (util.inspect(parse_expr ('++apple + 1 - - bad'), { colors: true, depth: 100 }));
   });
